decfc4eeb1c3121a8adfb00f297e5bca60170b91
Jeremiah Shugrue <jpshugrue@gmail.com>
2018-01-16 20:02:43 -0500

    Remove reference mats

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 const SnakeView = require('./snake-view');
 const $free = require("./freeDOM");
-// $(function () {
-//   var rootEl = $('.snake-game');
-//   new SnakeView(rootEl);
-// });
 
 $free(function () {
   const rootEl = $free('.snake-game');
